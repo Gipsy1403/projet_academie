b22d41eb07205dfe8493a942b92448cfa465cb31
Gipsy Rouaret <gipsy.rouaret@orange.fr>
2024-12-15 12:26:45 +0100

    ajout base de données

--- base de données/projet_academie (1).sql
+++ base de données/projet_academie (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le : ven. 13 déc. 2024 à 11:08
+-- Généré le : dim. 15 déc. 2024 à 12:25
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -32,10 +32,16 @@ CREATE TABLE `creature` (
   `nom` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `image_creature` text NOT NULL,
-  `createfiche` varchar(255) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `creature`
+--
+
+INSERT INTO `creature` (`id_creature`, `nom`, `description`, `image_creature`, `id_user`, `id_type`) VALUES
+(17, 'Eclair', 'n sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L\'avantage du Lorem Ipsum sur un texte générique comme ', '1734256824761', 8, 0);
 
 -- --------------------------------------------------------
 
@@ -87,10 +93,21 @@ CREATE TABLE `sort` (
   `id_sort` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `image_sort` text NOT NULL,
-  `create_fiche` varchar(255) NOT NULL,
   `id_element` int(11) NOT NULL,
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `sort`
+--
+
+INSERT INTO `sort` (`id_sort`, `nom`, `image_sort`, `id_element`, `id_user`) VALUES
+(27, 'Eclair', '173425373648.webp', 0, 8),
+(28, 'Eclair', '1734255247752.webp', 0, 8),
+(29, 'Eclair', '1734256709451.webp', 0, 8),
+(30, 'Elementaire d\'air', '1734260171131.webp', 0, 8),
+(31, 'Eclair', '1734260406749.webp', 0, 8),
+(32, 'Eclair', '1734261531710.webp', 0, 8);
 
 -- --------------------------------------------------------
 
@@ -125,6 +142,18 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `id_role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `name`, `password`, `id_role`) VALUES
+(8, 'anastasya', '$2y$10$Ve/hDjiGLW6Eng7.6jd2f.MNTHu.x0mKd62QKAbkAw1ntOR5gQGbS', 1),
+(9, 'kiril', '$2y$10$cCTAkST0m0er0Qyf8NAWau9EUyrF4e0dHkEkZs1MTReRx7.FgFqKi', 1),
+(10, 'anton', '$2y$10$Rex4tHZJAUExurBMxsVstetc945v3yxe/Zh9jjHU.qJgae65./F/i', 1),
+(11, 'irina', '$2y$10$lkZsC8wc4d7j9yTUT6zcIO259heUqabbftNUTnxkJ6PXS8OwCX.kK', 1),
+(12, 'jorgen', '$2y$10$p3j8MwboIwJAHDElysVrU.DPjUJYaIk9euVS4Y9yY28/Qt9Jx8I9K', 1),
+(13, 'kalindra', '$2y$10$sGrpa6gU.IcVjTM3T8zqXe/TLaRKwAJCn/peIc7hHLqyI3UjLPWtW', 1);
 
 -- --------------------------------------------------------
 
@@ -192,7 +221,7 @@ ALTER TABLE `user_element`
 -- AUTO_INCREMENT pour la table `creature`
 --
 ALTER TABLE `creature`
-  MODIFY `id_creature` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_creature` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `element`
@@ -210,7 +239,7 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT pour la table `sort`
 --
 ALTER TABLE `sort`
-  MODIFY `id_sort` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_sort` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT pour la table `type`
@@ -222,7 +251,7 @@ ALTER TABLE `type`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `user_element`
